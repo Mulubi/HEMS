78ad586b9eb84024da4af6560f827f697a8a8907
Mulubi <mulubiwarren@gmail.com>
2023-06-17 18:48:19 +0300

    Ammended missing routes

---
+++ backend/routes/technician.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const {getAllTechnicians,
+    getTechnicianById,
+    createTechnician,
+    updateTechnician,
+    deleteTechnician,
+} = require('../controllers/technician');
+router.route('/').get(getAllTechnicians).post(createTechnician);
+router.route('/:id').get(getTechnicianById).delete(deleteTechnician).patch(updateTechnician)
+
+module.exports = router;
+  
