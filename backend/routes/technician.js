ade39a412adcf7fcfdd185ffb43d3388056ba23b
Warren Wice <mulubiwarren@gmail.com>
2023-06-19 08:18:09 +0300

    Modified technician route

--- backend/routes/technician.js
+++ backend/routes/technician.js
@@ -10,9 +10,4 @@ const {getAllTechnicians,
 router.route('/').get(getAllTechnicians).post(createTechnician);
 router.route('/:id').get(getTechnicianById).delete(deleteTechnician).patch(updateTechnician)
 
-<<<<<<< HEAD
 module.exports = router;
-=======
-module.exports = router;
-  
->>>>>>> 78ad586b9eb84024da4af6560f827f697a8a8907
