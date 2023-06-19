9eefed28731a1d62382d2a64ea1572ff5e19fdc9
Warren Wice <mulubiwarren@gmail.com>
2023-06-19 08:00:29 +0300

    Working tree settings

---
+++ backend/routes/equipment.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+const {getAllEquipment,
+    createEquipment,
+    updateEquipment,
+    deleteEquipment,
+    getEquipment,
+    searchEquipment,
+    performMaintenance,
+    transferEquipment,
+    markEquipmentAsAvailable,
+    updateEquipmentStatus,
+    modifyMaintenanceRequest
+} = require('../controllers/equipment');
+router.route('/').get(getAllEquipment, searchEquipment).post(createEquipment);
+router.route('/:id').get(getEquipment).delete(deleteEquipment).patch(updateEquipment, modifyMaintenanceRequest);
+router.route('/:id/maintenance').post(performMaintenance);
+router.route('/:id/transfer').post(transferEquipment);
+router.route('/:id/Available').post(markEquipmentAsAvailable);
+router.route('/:id/status').patch(updateEquipmentStatus);
+
+module.exports = router;
