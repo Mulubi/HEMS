46c5f63384b1158ae7e836b9e96c7e3e7fa08584
Mulubi <mulubiwarren@gmail.com>
2023-06-25 12:16:12 +0300

    Remove backed dir

--- backend/README.md
+++
@@ -1,1 +0,0 @@
-Backend stuff contained here
--- backend/middleware/error-handler.js
+++
@@ -1,9 +0,0 @@
-const errorHandlerMiddleware = (err,req, res, next) => {
-    console.log(err);
-    if (res.headersSent) {
-        return next(err);
-    }
-    res.status(500).json({ error: 'Internal Server Error'});
-};
-
-module.exports = errorHandlerMiddleware;
